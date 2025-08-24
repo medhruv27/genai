86a7b1bd4cd76637ec9076febd5c6d5d2022ac7c
medhruv27 <dv21578@gmail.com>
2025-08-24 17:37:06 +0530

    chat rag

---
+++ 17_chat_rag.py
@@ -0,0 +1,62 @@
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY environment variable not set")
+
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+# Vector Embeddings
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="text-embedding-004",
+    google_api_key=api_key
+)
+
+vector_db = QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="learning_vectors",
+    embedding=embedding_model,
+    # force_recreate=True
+)
+
+# Take User Query
+query = input("> ")
+
+# Vector Similarity Search [query] in DB
+search_results = vector_db.similarity_search(
+    query=query
+)
+
+# print("search_results: ", search_results)
+
+context = "\n\n".join(
+    [f"Page Content: {result.page_content}\nPage Number: {result.metadata['page_label']}\nFile Location: {result.metadata['source']}" for result in search_results])
+
+SYSTEM_PROMPT = f"""
+    You are a helpful AI assistant who answers user query based on the available context retrieved from a PDF file along with page_contents and page number.
+
+    You should only answer the user based on the following context and navigate the user to open the right page number to know more.
+
+    Context:
+    {context}
+"""
+
+# print("SYSTEM_PROMPT: ", SYSTEM_PROMPT)
+
+chat_completion = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": query}
+    ],
+)
+
+print(f"🤖: {chat_completion.choices[0].message.content}")
